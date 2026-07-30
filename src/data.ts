fd734bb21cee62616d2a3c5071b6a22dc8ca3575
M0abl1 <104042903+M0abl1@users.noreply.github.com>
2026-07-30 08:29:04 -0300

    feat(categorias): adicione contas fixas

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,15 @@
-import type { Asset, Goal, Transaction } from './types'
+import type { Asset, Goal, Transaction } from "./types";
 
 export const categories = [
-  ['Mercado','🛒'], ['Moradia','🏠'], ['Delivery','🍕'], ['Combustível','⛽'], ['Academia','🏋️'], ['Farmácia','💊'], ['Lazer','🎬']
-] as const
-export const seedTransactions: Transaction[] = []
-export const seedAssets: Asset[] = []
-export const seedGoals: Goal[] = []
+  ["Mercado", "🛒"],
+  ["Moradia", "🏠"],
+  ["Contas fixas", "🧾"],
+  ["Delivery", "🍕"],
+  ["Combustível", "⛽"],
+  ["Academia", "🏋️"],
+  ["Farmácia", "💊"],
+  ["Lazer", "🎬"],
+] as const;
+export const seedTransactions: Transaction[] = [];
+export const seedAssets: Asset[] = [];
+export const seedGoals: Goal[] = [];
